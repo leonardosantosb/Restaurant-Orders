4c8fef84576ca8babbe9cc3ad87e8131e9b38714
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-07-20 15:02:52 -0300

    req 1 - testes ingredientes

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,36 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, restriction_map  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+
+    ingredient = Ingredient("Tomato")
+    assert isinstance(ingredient, Ingredient)
+
+    assert ingredient.restrictions == restriction_map().get(
+        "Tomato", set()
+        )
+
+    assert repr(ingredient) == f"Ingredient('{ingredient.name}')"
+
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Tomato")
+    assert ingredient1 == ingredient2
+    assert ingredient1 == ingredient1
+    assert hash(ingredient1) == hash(ingredient2)
+
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Potato")
+    assert hash(ingredient1) != hash(ingredient2)
+
+    assert ingredient.name == "Tomato"
+
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Potato")
+    assert ingredient1 != ingredient2
+
+    assert ingredient.restrictions == restriction_map().get(
+        "Tomato", set()
+        )
+
+    assert ingredient == ingredient
